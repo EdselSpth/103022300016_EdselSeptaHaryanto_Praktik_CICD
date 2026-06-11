54589ecfa5ac654dc69d7df99cbe534b9be8a98a
Edsel <edselspth@gmail.com>
2026-06-11 07:12:35 +0700

    fix: [ESH] memperbaiki agar quality gate bisa jalan

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -46,9 +46,5 @@ public class Counter {
 	public int getCount() {
 		return count;
 	}
-
-	public void setCount(int count) {
-		this.count = count;
-	}
 	
 }
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -13,10 +13,6 @@ public class Driver {
 		counter.decrement();
 		
 		System.out.println("Current count:" + counter.getCount());
-
-		counter.setCount(5);
-
-		System.out.println("Current count:" + counter.getCount());
 		
 	}
 
