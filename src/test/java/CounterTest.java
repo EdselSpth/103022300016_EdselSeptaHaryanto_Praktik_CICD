b33487289f6c0ccca82924ccddb3b02423399267
Edsel <edselspth@gmail.com>
2026-06-11 07:20:12 +0700

    fix: [ESH] memperbaiki continous deployment yang gagal

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -35,16 +35,4 @@ public class CounterTest {
 			assertEquals(testCounter.getCount(), i * -1);
 		}
 	}
-
-	@Test
-	public void testDecrementInvalid() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
-			testCounter.decrement();
-			assertEquals(testCounter.getCount(), i * 8);
-		}
-	}
-
-
 }
